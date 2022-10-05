87e20e6eb33a9301644c869033e8ef61b9e6e506
xwang-edu <wangxuege.edu@gmail.com>
2022-10-05 17:34:26 +0800

    command fixed (final)
    able to give 180 turn

--- mdp_python_algo/robot_pathfinder/models/command.py
+++ mdp_python_algo/robot_pathfinder/models/command.py
@@ -12,11 +12,9 @@ class Command:
         return self.__str__()
 
     def __str__(self):
-        if self.repeat>10:
-            return f'{self.move.name}{self.repeat*10}'
-        else:
-            if self.move.name == "l" or self.move.name == "r" or self.move.name == "m" or self.move.name == "o":
-                return f'{self.move.name}{self.zero}{90}'
-            return f'{self.move.name}{self.zero}{self.repeat*10}'
+        if self.move.name == "l" or self.move.name == "r" or self.move.name == "m" or self.move.name == "o":
+            return f'{self.move.name}{str(self.repeat*90).zfill(3)}'
+        return f'{self.move.name}{str(self.repeat*10).zfill(3)}'
+        
     def inc_repeat(self):
         self.repeat += 1
